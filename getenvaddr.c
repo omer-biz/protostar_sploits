743ec196b2984404f4ced1559d3b09230c2d0882
Omer Abdulaziz <omerabdi@protonmail.com>
2020-05-15 14:03:18 +0300

    get's env var addr

---
+++ getenvaddr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv) {
+	char *ptr;
+
+	
+	ptr = getenv(argv[1]);
+	ptr += (strlen(argv[0]) - strlen(argv[2])) * 2;
+
+	printf("%p\n", ptr);
+}
